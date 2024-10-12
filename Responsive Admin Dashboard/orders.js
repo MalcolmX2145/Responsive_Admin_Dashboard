a598ae52c3502bec1e60818ab31beb841d0abbad
Malcolm Ekajja <130965426+MalcolmX2145@users.noreply.github.com>
2024-10-12 13:56:38 +0300

    Add files via upload

---
+++ Responsive Admin Dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Ferrari SF24 Tutorial",
+    productNumber: "85743",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "Formula 1 Full Course",
+    productNumber: "97245",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Racing Line Tutorial",
+    productNumber: "36452",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
